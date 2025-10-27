77f68a18ebae4f79e44c91ceb90fea7b58d77667
cl-journeyor <genericobject711@yahoo.com>
2025-10-27 09:11:52 -0700

    New functions

--- src/jnorlib/core.clj
+++ src/jnorlib/core.clj
@@ -1,6 +1,33 @@
-(ns jnorlib.core)
+(ns jnorlib.core
+  "Supplementary API.")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn between?
+  "Checks whether `lower <= c <= upper` is true."
+  [c lower upper]
+  (and
+   (>= (compare c lower) 0)
+   (<= (compare c upper) 0)))
+
+(defn lines
+  "Returns a sequence with the lines from a file read with the provided
+  `java.io.BufferedReader` as `in`.
+  See also [[clojure.java.io/reader]]."
+  [in]
+  (->> (repeat #(.readLine in))
+       (map #(%))
+       (take-while #(not (nil? %)))))
+
+(defn matches?
+  "Checks whether the string `s` matches against the RegEx Pattern `re`."
+  [^String s re]
+  (-> (re-matcher re s)
+      re-find
+      nil?
+      not))
+
+(defn throws?
+  "Invokes the provided `f` to check whether it throws an exception."
+  [f]
+  (try
+    (not (any? (f)))
+    (catch Exception _ true)))
